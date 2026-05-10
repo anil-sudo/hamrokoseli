312c3cfb7b7123454babbd52b38c667dce7ddf6b
Anil S. <as.unique2023@gmail.com>
2026-05-10 12:42:49 -0700

    chore(app): added Master Layout wip (#1)

---
+++ resources/views/components/layout-master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name', 'HamroKoseli') }}</title>
+    
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+
+    @fluxAppearance
+</head>
+<body>
+    {{ $slot }}
+
+    @livewireScripts
+    @fluxScripts
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\PageController;
 
-Route::view('/', 'welcome')->name('home');
+Route::middleware(['web'])->group(function () {
+    Route::get('/', [PageController::class, 'index'])->name('home');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
