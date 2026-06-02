48941e6e024862f4b9df537031dc5b70f3b17f03
anil-sudo <as.unique2023@gmail.com>
2026-06-02 11:14:02 -0700

    fix code style

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,38 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PageController extends Controller
 {
     public function home()
     {
         return view('welcome');
     }
+
     public function categories()
     {
         return view('categories');
     }
+
     public function shop()
     {
         return view('shop');
     }
+
     public function new_arrival()
     {
         return view('new_arrival');
     }
+
     public function todays_deals()
     {
         return view('todays-deals');
     }
+
     public function featured_products()
     {
         return view('featured-products');
     }
+
     public function top_sellers()
     {
         return view('top-sellers');
     }
+
     public function about_us()
     {
         return view('about');
