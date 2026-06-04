9a048384a67790a63318abb9fc192b334ea13d99
Babisa katwal <babikatwal02@gmail.com>
2026-06-04 22:41:08 +0545

    Improve sidebar and dashboard design  (#10)
    * sidebar and seller dashboard UI
    
    * Update sidebar and seller-dashboard
    
    * fix lint script
    
    * Fix pint formatting
    
    * improve sidebar/topbar/dashboard design
    
    * fix remaining changes

--- resources/views/components/seller_topbar.blade.php
+++ resources/views/components/seller_topbar.blade.php
@@ -1,5 +1,5 @@
             <header
-                class="sticky top-0 z-30 w-full bg-(--primary-color) border-b border-(--primary-color)/15% shadow-sm">
+                class="sticky top-0 z-30 w-full bg-(--primary-color) border-b border-(--primary-color)/15 shadow-sm backdrop-blur-md">
                 <div class="flex items-center gap-3 w-full px-4 md:px-8 py-3 justify-between">
 
                     <button id="menu-btn" class="md:hidden text-white mr-2">
@@ -18,14 +18,22 @@
                     {{-- Right Icons --}}
                     <div class="flex items-center gap-3">
                         {{-- Notification Bell --}}
-                        <a href="" class="relative p-1 text-(--text-light) hover:text-(--hover-color)">
-                            <i class="fas fa-bell text-lg"></i>
-                            <span class="absolute top-0 right-0 w-2 h-2"></span>
+                        <a href="#"
+                            class="relative p-3 text-white hover:bg-white/10 hover:text-(--hover-color) active:bg-white/20 rounded-3xl transition-all duration-200 group">
+                            <i class="fas fa-bell text-xl"></i>
+                            <span class="absolute top-2 right-2 flex h-5 w-5 items-center justify-center">
+                                <span
+                                    class="animate-ping absolute inline-flex h-4 w-4 rounded-full bg-red-400 opacity-75"></span>
+                                <span
+                                    class="relative inline-flex rounded-full h-4 w-4 bg-red-500 text-[10px] font-medium items-center justify-center ring-2 ring-(--primary-color)">
+                                    3
+                                </span>
+                            </span>
                         </a>
 
                         {{-- Profile --}}
                         <a href=""
-                            class="w-8 h-8 bg-(--text-light) rounded-full flex items-center justify-center hover:bg-(--hover-color) transition">
+                            class="w-8 h-8 bg-(--text-light) rounded-full flex items-center justify-center hover:bg-(--hover-color) cursor-pointer hover:scale-110 transition-transform">
                             <i class="fas fa-user text-(--text-color)"></i>
                         </a>
                     </div>
