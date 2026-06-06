f18a027ed2e69c4e1e0ec635341f6be978290e12
anil-sudo <as.unique2023@gmail.com>
2026-06-06 09:46:32 -0700

    my local changes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,6 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->profile()
             ->login()
-            ->registration()
             ->colors([
                 'primary' => Color::Amber,
             ])
