8c7a5373d22406f31fb9fbe21705e28d458b951d
Nishan Rai <nishansampang9@gmail.com>
2026-06-09 09:06:35 +0545

    add cards

--- resources/views/todays-deals.blade.php
+++ resources/views/todays-deals.blade.php
@@ -1,3 +1,10 @@
 <x-frontend-layout>
+
     <h1 className="text-2xl font-bold text-gray-800 text-center">Today's Deals  will be displayed here</h1>
+    <div class="today-deals-container">
+        <div class = "today-deal"> card</div>
+        <div class = "today-deal"> card</div>
+        <div class = "today-deal"> card</div>
+
+    </div>
 </x-frontend-layout>
