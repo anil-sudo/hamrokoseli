d366c86f116b4d8402050d0bbce65150acc7aa13
Nishan Rai <nishansampang9@gmail.com>
2026-06-09 09:53:49 +0545

    make place for product Image and its info in today's deal page

--- resources/views/todays-deals.blade.php
+++ resources/views/todays-deals.blade.php
@@ -2,13 +2,61 @@
 
     <h1 className="text-2xl font-bold text-gray-800 text-center">Today's Deals  will be displayed here</h1>
     <div class="today-deals-container">
-        <div class = "today-deal"> card</div>
-        <div class = "today-deal"> card</div>
-        <div class = "today-deal"> card</div>
-        <div class = "today-deal"> card</div>
-        <div class = "today-deal"> card</div>
-        <div class = "today-deal"> card</div>
-        <div class = "today-deal"> card</div>
+        <div class = "today-deal" id="deal-1">
+            <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
+
+            <div class="deal-info">
+                <h2 class="deal-title">Sample Product Name</h2>
+                <p class="deal-price">Rs. 1,234</p>
+                <p class="deal-discount">20% off</p>
+                <p class="deal-desc">Short description of the product goes here. Highlight features or promos.</p>
+            </div>
+        </div>
+        <div class = "today-deal" id="deal-2">
+            <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
+            <div class="deal-info">
+                <h2 class="deal-title">Sample Product Name</h2>
+                <p class="deal-price">Rs. 1,234</p>
+                <p class="deal-discount">20% off</p>
+                <p class="deal-desc">Short description of the product goes here. Highlight features or promos.</p>
+            </div>
+        </div>
+        <div class = "today-deal" id="deal-3">
+            <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
+            <div class="deal-info">
+                <h2 class="deal-title">Sample Product Name</h2>
+                <p class="deal-price">Rs. 1,234</p>
+                <p class="deal-discount">20% off</p>
+                <p class="deal-desc">Short description of the product goes here. Highlight features or promos.</p>
+            </div>
+        </div>
+        <div class = "today-deal" id="deal-4">
+            <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
+            <div class="deal-info">
+                <h2 class="deal-title">Sample Product Name</h2>
+                <p class="deal-price">Rs. 1,234</p>
+                <p class="deal-discount">20% off</p>
+                <p class="deal-desc">Short description of the product goes here. Highlight features or promos.</p>
+            </div>
+        </div>
+        <div class = "today-deal" id="deal-5">
+            <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
+            <div class="deal-info">
+                <h2 class="deal-title">Sample Product Name</h2>
+                <p class="deal-price">Rs. 1,234</p>
+                <p class="deal-discount">20% off</p>
+                <p class="deal-desc">Short description of the product goes here. Highlight features or promos.</p>
+            </div>
+        </div>
+        <div class = "today-deal" id="deal-6">
+            <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
+            <div class="deal-info">
+                <h2 class=" deal-title">Sample Product Name</h2>
+                <p class=" deal-price">Rs. 1,234</p>
+                <p class=" deal-discount">20% off</p>
+                <p class=" deal-desc">Short description of the product goes here. Highlight features or promos.</p>
+            </div>
+        </div>
 
     </div>
 </x-frontend-layout>
