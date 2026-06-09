42e418fbfcebe87eded20312c51f2bcb77352232
Nishan Rai <nishansampang9@gmail.com>
2026-06-09 13:04:22 +0545

    add images

--- resources/views/todays-deals.blade.php
+++ resources/views/todays-deals.blade.php
@@ -3,7 +3,7 @@
     <h1 className="text-2xl font-bold text-gray-800 text-center">Today's Deals  will be displayed here</h1>
     <div class="today-deals-container">
         <div class = "today-deal" id="deal-1">
-            <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
+            <img src="{{ asset('images/Pottery.png') }}" alt="Sample product" class="deal-image">
 
             <div class="deal-info">
                 <h2 class="deal-title">Sample Product Name</h2>
@@ -12,8 +12,9 @@
                 <p class="deal-desc">Short description of the product goes here. Highlight features or promos.</p>
             </div>
         </div>
-        <div class = "today-deal" id="deal-2">
-            <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
+
+      <div class = "today-deal" id="deal-2">
+            <img src="{{ asset('images/SunGlasS.png') }}" alt="Sample product" class="deal-image">
             <div class="deal-info">
                 <h2 class="deal-title">Sample Product Name</h2>
                 <p class="deal-price">Rs. 1,234</p>
@@ -21,6 +22,7 @@
                 <p class="deal-desc">Short description of the product goes here. Highlight features or promos.</p>
             </div>
         </div>
+
         <div class = "today-deal" id="deal-3">
             <img src="/images/sample-product.jpg" alt="Sample product" class="deal-image">
             <div class="deal-info">
@@ -56,7 +58,7 @@
                 <p class=" deal-discount">20% off</p>
                 <p class=" deal-desc">Short description of the product goes here. Highlight features or promos.</p>
             </div>
-        </div>
+        </div> 
 
     </div>
 </x-frontend-layout>
