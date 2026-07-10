f7977969bee1b741fd5f2b3fe24b2da75d04bf10
Nishan Rai <nishansampang9@gmail.com>
2026-07-10 23:12:53 +0545

    add register

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,7 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->profile()
             ->login()
-            // ->registration(\App\Filament\Auth\Register::class)
+            ->registration(\App\Filament\Auth\Register::class)
             ->colors([
                 'primary' => Color::Amber,
             ])
